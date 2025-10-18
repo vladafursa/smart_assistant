4724d66ffadba8bee84ea8cdb2e28602616e706b
T0337485 <t0337485@my.ntu.ac.uk>
2025-10-19 00:52:27 +0300

    added settings

---
+++ config/__init__.py
@@ -0,0 +1,9 @@
+from .settings import (
+    PINECONE_KEY,
+    HUGGINGFACE_KEY,
+    LLM_KEY,
+    INDEX_NAME,
+    NAMESPACE,
+    CHUNK_SIZE,
+    CHUNK_OVERLAP,
+)
---
+++ config/settings.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_KEY = os.getenv("PINECONE_KEY")
+HUGGINGFACE_KEY = os.getenv("HUGGINGFACE_KEY")
+LLM_KEY = os.getenv("LLM_KEY")
+
+INDEX_NAME = "smart_support"
+NAMESPACE = "default"
+CHUNK_SIZE = 500
+CHUNK_OVERLAP = 100
