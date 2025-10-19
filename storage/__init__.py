e58b23fd400aba3f02a1cd477b55736befc68714
T0337485 <t0337485@my.ntu.ac.uk>
2025-10-20 00:41:32 +0300

    added imports

---
+++ storage/__init__.py
@@ -0,0 +1,1 @@
+from pinecone_storage import split_texts, prepare_chunks_for_indexing, init_index, upsert_vectors, process_all_files
--- storage/pinecone_storage/__init__.py
+++ storage/pinecone_storage/__init__.py
@@ -1,1 +1,2 @@
 from .preparation import split_texts, prepare_chunks_for_indexing
+from .insertion import init_index, upsert_vectors, process_all_files
