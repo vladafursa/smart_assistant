d844342914341028bcc7d41d778ddb03d091da4b
T0337485 <t0337485@my.ntu.ac.uk>
2025-10-19 23:48:04 +0300

    added getting embeddings featire

---
+++ ml/embedder.py
@@ -0,0 +1,27 @@
+import requests
+import os
+from config import HUGGINGFACE_KEY
+
+API_URL = "https://router.huggingface.co/hf-inference/models/intfloat/multilingual-e5-large/pipeline/feature-extraction"
+headers = {
+    "Authorization": f"Bearer {HUGGINGFACE_KEY}",
+}
+
+def get_embeddings(texts):
+    if isinstance(texts, str):
+        texts = [texts]
+
+    payload = {"inputs": texts}
+    try:
+        response = requests.post(API_URL, headers=headers, json=payload)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP error: {e}")
+        print("Response text:", response.text)
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+    except ValueError:
+        print("Failed to decode JSON. Raw response:")
+        print(response.text)
+
