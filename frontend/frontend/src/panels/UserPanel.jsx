efc9a1d58d545c7375ae1d372eff807ccb74bdd0
T0337485 <t0337485@my.ntu.ac.uk>
2025-11-07 02:34:52 +0300

    added sending user's message

--- frontend/frontend/src/panels/UserPanel.jsx
+++ frontend/frontend/src/panels/UserPanel.jsx
@@ -1,28 +1,22 @@
 import Message from "../components/Message";
 import Form from "../components/Form";
-
-const chatMessages = [{
-  sender: "user",
-  message: "message"
-},
-{
-  sender: "assistant",
-  message: "message"
-}];
-
-const chatMessageComponents = chatMessages.map((chatMessage)=>{
-  return(
-     <Message sender={chatMessage.sender} 
-     text={chatMessage.message} />
-  );
-});
+import { useEffect, useState } from "react";
 
 const UserPanel = ({ message }) => {
+  const [chatMessages, setChatMessages] = useState([
+    { sender: "assistant", message: "Hello! How can I help you?" }
+  ]);
+  const handleSend = (text, sender) => {
+  setChatMessages((prev) => [...prev, { sender, message: text }]);
+  };
+
   return (
     <div className="flex flex-col h-full">
-      {chatMessageComponents}
+       {chatMessages.map((chatMessage, i) => (
+          <Message key={i} sender={chatMessage.sender} text={chatMessage.message} />
+        ))}
       <div className="mt-auto">
-        <Form />
+        <Form sender="user" onSend={(text) => handleSend(text, "user")} />
       </div>
     </div>
   );
