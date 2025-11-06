4b2f6de55cf793d2618a77e7ce7ef79c64223a4b
T0337485 <t0337485@my.ntu.ac.uk>
2025-11-07 02:03:08 +0300

    transferred example messages to array

--- frontend/frontend/src/panels/UserPanel.jsx
+++ frontend/frontend/src/panels/UserPanel.jsx
@@ -1,11 +1,26 @@
 import Message from "../components/Message";
 import Form from "../components/Form";
 
+const chatMessages = [{
+  sender: "user",
+  message: "message"
+},
+{
+  sender: "assistant",
+  message: "message"
+}];
+
+const chatMessageComponents = chatMessages.map((chatMessage)=>{
+  return(
+     <Message sender={chatMessage.sender} 
+     text={chatMessage.message} />
+  );
+});
+
 const UserPanel = ({ message }) => {
   return (
     <div className="flex flex-col h-full">
-      <Message sender="user" text={message} />
-
+      {chatMessageComponents}
       <div className="mt-auto">
         <Form />
       </div>
